e01b3a5d0c9b40bb465d7fd0cdc54f1392860e7f
subrata <subrata@appsmith.com>
2025-03-13 10:30:37 +0000

    Add a limit input

--- modules/JSModule1/jsobjects/JSModule1/JSModule1.js
+++ modules/JSModule1/jsobjects/JSModule1/JSModule1.js
@@ -1,10 +1,10 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	myFun1 () {
+	myFun1 (limit = 7) {
 		//	write code here
 		//	this.myVar1 = [1,2,3]
-		return GetUsersByLimit.run()
+		return GetUsersByLimit.run({ limit: limit })
 	},
 	async myFun2 () {
 		//	use async-await or promises
